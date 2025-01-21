e3eb080c33e801ca5030d1adf70816e4dffb666a
evanm510 <evanm510@gmail.com>
2025-01-20 19:14:40 -0800

    use asset viewer to display resume pdf

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 
 import { NavBar } from "@/components/navbar";
+import AssetViewer from "@/components/AssetViewer/AssetViewer";
 
 export const metadata: Metadata = {
   title: "Evan Monroe",
@@ -18,6 +19,7 @@ export default function RootLayout({
         {" "}
         <NavBar />
         {children}
+        <AssetViewer />
       </body>
     </html>
   );
