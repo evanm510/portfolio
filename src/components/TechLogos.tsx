070bc8d843d72840c5a00ccf5ecac5dd88b002cf
evanm510 <evanm510@gmail.com>
2025-01-21 14:34:33 -0800

    adjust tech logo margin

--- src/components/TechLogos.tsx
+++ src/components/TechLogos.tsx
@@ -37,7 +37,7 @@ export const TechLogoList = ({
   size: number;
 }) => {
   return (
-    <div className="flex flex-row gap-6 items-center">
+    <div className="flex flex-row gap-10 items-center">
       {logos.map((logo, index) => (
         <TechLogo
           key={index}
