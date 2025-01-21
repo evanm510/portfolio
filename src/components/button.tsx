0400c3a5e75af3440b4479415566941747a80c2f
evanm510 <evanm510@gmail.com>
2025-01-20 16:16:46 -0800

    add button component

---
+++ src/components/button.tsx
@@ -0,0 +1,18 @@
+"use client";
+
+export const Button = ({
+  children,
+  onClick,
+}: {
+  children: string;
+  onClick: () => void;
+}) => {
+  return (
+    <div
+      className="inline-block bg-button px-4 py-2 text-gray rounded-lg cursor-pointer hover:scale-105 transform transition-transform duration-300"
+      onClick={() => onClick()}
+    >
+      {children}
+    </div>
+  );
+};
