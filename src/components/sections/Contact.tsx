e1ea5eb9d459aaf98e9a924cd6323f76e94a503d
evanm510 <evanm510@gmail.com>
2025-01-22 14:51:40 -0800

    fix contact navigation

--- src/components/sections/Contact.tsx
+++ src/components/sections/Contact.tsx
@@ -2,7 +2,7 @@ import ContactForm from "../ContactForm";
 
 export default () => {
   return (
-    <section id="experience" className="scroll-mt-20  mb-20">
+    <section id="contact" className="scroll-mt-20  mb-20">
       <div className="flex flex-col gap-4 items-center">
         <div className="text-3xl mx-auto">Contact</div>
         <div className="w-[90%] bg-secondary p-6 rounded-lg">
