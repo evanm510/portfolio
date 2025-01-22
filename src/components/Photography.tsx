cf067f678da1c8e62299882d76b02ee2fbcecc28
evanm510 <evanm510@gmail.com>
2025-01-21 17:03:29 -0800

    add photography section

---
+++ src/components/Photography.tsx
@@ -0,0 +1,71 @@
+import { Button } from "./Button";
+import { ImageSlider } from "./ImageSlider";
+
+const images = [
+  {
+    imageSrc: "photography_slider_images/portfolio_image_slider-1.jpg",
+    alt: "Portfolio image slider 1",
+  },
+  {
+    imageSrc: "photography_slider_images/portfolio_image_slider-2.jpg",
+    alt: "Portfolio image slider 2",
+  },
+  {
+    imageSrc: "photography_slider_images/portfolio_image_slider-3.jpg",
+    alt: "Portfolio image slider 3",
+  },
+  {
+    imageSrc: "photography_slider_images/portfolio_image_slider-6.jpg",
+    alt: "Portfolio image slider 6",
+  },
+  {
+    imageSrc: "photography_slider_images/portfolio_image_slider-7.jpg",
+    alt: "Portfolio image slider 7",
+  },
+  {
+    imageSrc: "photography_slider_images/portfolio_image_slider-8.jpg",
+    alt: "Portfolio image slider 8",
+  },
+  {
+    imageSrc: "photography_slider_images/portfolio_image_slider-9.jpg",
+    alt: "Portfolio image slider 9",
+  },
+  {
+    imageSrc: "photography_slider_images/portfolio_image_slider-10.jpg",
+    alt: "Portfolio image slider 11",
+  },
+  {
+    imageSrc: "photography_slider_images/portfolio_image_slider-11.jpg",
+    alt: "Portfolio image slider 11",
+  },
+  {
+    imageSrc: "photography_slider_images/portfolio_image_slider-12.jpg",
+    alt: "Portfolio image slider 12",
+  },
+  {
+    imageSrc: "photography_slider_images/portfolio_image_slider-13.jpg",
+    alt: "Portfolio image slider 13",
+  },
+  {
+    imageSrc: "photography_slider_images/portfolio_image_slider-14.jpg",
+    alt: "Portfolio image slider 14",
+  },
+  {
+    imageSrc: "photography_slider_images/portfolio_image_slider-15.jpg",
+    alt: "Portfolio image slider 15",
+  },
+];
+
+export default () => {
+  return (
+    <div className="w-[90%] bg-secondary p-6 rounded-lg">
+      <div className="flex justify-center mb-4">
+        <div className="text-2xl">Photography</div>
+      </div>
+      <ImageSlider images={images} aspectRatio="16/9" />
+      <div className="flex justify-center mt-4">
+        <Button onClick={() => {}}>View Full Gallery</Button>
+      </div>
+    </div>
+  );
+};
--- src/components/sections/OtherInterests.tsx
+++ src/components/sections/OtherInterests.tsx
@@ -1,10 +1,16 @@
+import Photography from "../Photography";
+
 export default () => {
   return (
     <section
       id="other-interests"
-      className="scroll-mt-20 h-screen flex items-center justify-center"
+      className="scroll-mt-20 flex items-center justify-center"
     >
-      <div>Hobbies</div>
+      <div className="flex flex-col justify-center">
+        <div className="flex justify-center">
+          <Photography />
+        </div>
+      </div>
     </section>
   );
 };
