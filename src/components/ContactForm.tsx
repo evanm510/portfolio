5d4f5b9d836bf777c1d24aa2fbf864f28aea024b
evanm510 <evanm510@gmail.com>
2025-01-22 14:47:07 -0800

    add initial contact form

---
+++ src/components/ContactForm.tsx
@@ -0,0 +1,74 @@
+import React, { useState } from "react";
+import { Button } from "./Button";
+
+export default () => {
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    message: "",
+  });
+
+  const handleChange = (
+    e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const handleSubmit = () => {
+    if (!formData.name || !formData.email || !formData.message) {
+      alert("Please fill out all fields.");
+      return;
+    }
+
+    console.log("Form Data Submitted:", formData);
+    setFormData({ name: "", email: "", message: "" });
+    alert("Thank you! Your message has been sent.");
+  };
+
+  return (
+    <div className="flex items-center justify-center text-white">
+      <div className="max-w-md w-full space-y-4">
+        <h1 className="text-lg font-semibold text-center">
+          Have a question or want to work together? Leave your details and I'll
+          get back to you as soon as possible.
+        </h1>
+        <form className="space-y-4" onSubmit={(e) => e.preventDefault()}>
+          <div>
+            <input
+              type="text"
+              name="name"
+              placeholder="Name"
+              value={formData.name}
+              onChange={handleChange}
+              className="w-full p-3 bg-gray-800 text-white rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+            />
+          </div>
+          <div>
+            <input
+              type="email"
+              name="email"
+              placeholder="Email"
+              value={formData.email}
+              onChange={handleChange}
+              className="w-full p-3 bg-gray-800 text-white rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+            />
+          </div>
+          <div>
+            <textarea
+              name="message"
+              placeholder="Message"
+              rows={4}
+              value={formData.message}
+              onChange={handleChange}
+              className="w-full p-3 bg-gray-800 text-white rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+            ></textarea>
+          </div>
+          <div>
+            <Button onClick={handleSubmit}>Submit</Button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
