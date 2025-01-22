8525bd74b420120eab2e715f83fd1835f871ee41
evanm510 <evanm510@gmail.com>
2025-01-21 17:23:32 -0800

    fix photography section

--- src/components/sections/OtherInterests.tsx
+++ src/components/sections/OtherInterests.tsx
@@ -1,4 +1,5 @@
 import Photography from "../Photography";
+import Music from "../Music";
 
 export default () => {
   return (
@@ -6,9 +7,13 @@ export default () => {
       id="other-interests"
       className="scroll-mt-20 flex items-center justify-center"
     >
-      <div className="flex flex-col justify-center">
+      <div className="flex flex-col justify-center align-center gap-6">
+        <div className="text-3xl mx-auto">Other Interests</div>
         <div className="flex justify-center">
           <Photography />
+        </div>
+        <div className="flex justify-center">
+          <Music />
         </div>
       </div>
     </section>
