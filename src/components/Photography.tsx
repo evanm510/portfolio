ec1dfd04e8463eea8e5366781811aedb354bedc0
evanm510 <evanm510@gmail.com>
2025-01-22 17:21:51 -0800

    use new images in portfolio album

--- src/components/Photography.tsx
+++ src/components/Photography.tsx
@@ -4,71 +4,9 @@ import React, { useState } from "react";
 import { ImageGallery } from "react-image-grid-gallery";
 import { Button } from "./Button";
 import { ImageSlider } from "./ImageSlider";
+import { sliderAlbumImages, fullAlbumImages } from "@/data/images";
 
-const images = [
-  {
-    imageSrc: "photography_slider_images/portfolio_image_slider-1.jpg",
-    alt: "Portfolio image slider 1",
-  },
-  {
-    imageSrc: "photography_slider_images/portfolio_image_slider-2.jpg",
-    alt: "Portfolio image slider 2",
-  },
-  {
-    imageSrc: "photography_slider_images/portfolio_image_slider-3.jpg",
-    alt: "Portfolio image slider 3",
-  },
-  {
-    imageSrc: "photography_slider_images/portfolio_image_slider-4.jpg",
-    alt: "Portfolio image slider 4",
-  },
-  {
-    imageSrc: "photography_slider_images/portfolio_image_slider-5.jpg",
-    alt: "Portfolio image slider 5",
-  },
-  {
-    imageSrc: "photography_slider_images/portfolio_image_slider-6.jpg",
-    alt: "Portfolio image slider 6",
-  },
-  {
-    imageSrc: "photography_slider_images/portfolio_image_slider-7.jpg",
-    alt: "Portfolio image slider 7",
-  },
-  {
-    imageSrc: "photography_slider_images/portfolio_image_slider-8.jpg",
-    alt: "Portfolio image slider 8",
-  },
-  {
-    imageSrc: "photography_slider_images/portfolio_image_slider-9.jpg",
-    alt: "Portfolio image slider 9",
-  },
-  {
-    imageSrc: "photography_slider_images/portfolio_image_slider-10.jpg",
-    alt: "Portfolio image slider 10",
-  },
-  {
-    imageSrc: "photography_slider_images/portfolio_image_slider-11.jpg",
-    alt: "Portfolio image slider 11",
-  },
-  {
-    imageSrc: "photography_slider_images/portfolio_image_slider-12.jpg",
-    alt: "Portfolio image slider 12",
-  },
-  {
-    imageSrc: "photography_slider_images/portfolio_image_slider-13.jpg",
-    alt: "Portfolio image slider 13",
-  },
-  {
-    imageSrc: "photography_slider_images/portfolio_image_slider-14.jpg",
-    alt: "Portfolio image slider 14",
-  },
-  {
-    imageSrc: "photography_slider_images/portfolio_image_slider-15.jpg",
-    alt: "Portfolio image slider 15",
-  },
-];
-
-const imagesArray = images.map((img, index) => ({
+const fullAlbumImagesArray = fullAlbumImages.map((img, index) => ({
   id: `image${index + 1}`,
   alt: img.alt,
   caption: img.alt,
@@ -93,7 +31,7 @@ export default function Photography() {
         <div className="text-2xl">Photography</div>
       </div>
 
-      <ImageSlider images={images} aspectRatio="16/9" />
+      <ImageSlider images={sliderAlbumImages} aspectRatio="16/9" />
 
       <div className="flex justify-center mt-4">
         <Button onClick={openModal}>View Full Gallery</Button>
@@ -112,7 +50,7 @@ export default function Photography() {
               &times;
             </button>
             <ImageGallery
-              imagesInfoArray={imagesArray}
+              imagesInfoArray={fullAlbumImagesArray}
               columnCount={"auto"}
               columnWidth={500}
               gapSize={24}
