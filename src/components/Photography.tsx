5aca011ef4b161ad38af51157ac82fdd7dd96fa7
evanm510 <evanm510@gmail.com>
2025-02-19 10:31:38 -0800

    paginate images

--- src/components/Photography.tsx
+++ src/components/Photography.tsx
@@ -4,19 +4,29 @@ import React, { useState } from "react";
 import { ImageGallery } from "react-image-grid-gallery";
 import { Button } from "./Button";
 import { ImageSlider } from "./ImageSlider";
-import { sliderAlbumImages, fullAlbumImages } from "@/data/images";
+import { sliderAlbumImages } from "@/data/images";
 import { IconButton } from "@radix-ui/themes";
 import { Cross1Icon } from "@radix-ui/react-icons";
 
-const fullAlbumImagesArray = fullAlbumImages.map((img, index) => ({
-  id: `image${index + 1}`,
-  alt: img.alt,
-  caption: "",
-  src: img.imageSrc,
-}));
+const generateImageArray = (
+  startIndex: number,
+  count: number,
+  totalImages: number = 84
+) => {
+  return Array.from({ length: totalImages }, (_, index) => {
+    const number = index + 1;
+    return {
+      id: `image${number}`,
+      alt: `Portfolio photo ${number}`,
+      caption: "",
+      src: `/portfolio_album/portfolio_photo-${number}.jpg`,
+    };
+  }).slice(startIndex, startIndex + count);
+};
 
 export default function Photography() {
   const [isModalOpen, setIsModalOpen] = useState(false);
+  const [loadCount, setLoadCount] = useState(1);
 
   const openModal = () => setIsModalOpen(true);
   const closeModal = () => setIsModalOpen(false);
@@ -24,6 +34,12 @@ export default function Photography() {
   const handleOverlayClick = (e: React.MouseEvent<HTMLDivElement>) => {
     if (e.target === e.currentTarget) {
       closeModal();
+    }
+  };
+
+  const handleLoadMore = () => {
+    if (loadCount < 3) {
+      setLoadCount(loadCount + 1);
     }
   };
 
@@ -51,12 +67,21 @@ export default function Photography() {
               </IconButton>
             </div>
             <div className="p-4">
-              <ImageGallery
-                imagesInfoArray={fullAlbumImagesArray}
-                columnCount={"auto"}
-                columnWidth={500}
-                gapSize={24}
-              />
+              {[...Array(loadCount)].map((_, index) => (
+                <div key={index} className="mb-8">
+                  <ImageGallery
+                    imagesInfoArray={generateImageArray(index * 20, 20)}
+                    columnCount={"auto"}
+                    columnWidth={500}
+                    gapSize={24}
+                  />
+                </div>
+              ))}
+              {loadCount < 3 && (
+                <div className="flex justify-center mt-4">
+                  <Button onClick={handleLoadMore}>Load More</Button>
+                </div>
+              )}
             </div>
           </div>
         </div>
