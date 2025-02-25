a6f8a6eb7e39fda29d7460e64db53b1808e06375
evanm510 <evanm510@gmail.com>
2025-02-25 09:32:00 -0800

    only display learn more btn when used

--- src/components/ExperienceSection.tsx
+++ src/components/ExperienceSection.tsx
@@ -12,8 +12,8 @@ export default function ExperienceSection({
   experienceData,
 }: ExperienceSectionProps) {
   return (
-    <div className="w-[90%] w-[90%] max-w-[1400px] bg-card shadow p-6 rounded-lg">
-      <div className="text-heading w-100 flex flex-col md:flex-row justify-between mb-4">
+    <div className="w-[90%] max-w-[1400px] bg-card shadow p-6 rounded-lg">
+      <div className="text-heading w-full flex flex-col md:flex-row justify-between mb-4">
         <div className="flex flex-col md:flex-row md:items-center gap-2">
           <div className="text-2xl">{experienceData.title}</div>
           <div className="hidden md:block">-</div>
@@ -40,16 +40,18 @@ export default function ExperienceSection({
           <ImageSlider images={experienceData.images} />
         </div>
       </div>
-      <div className="pt-4 flex flex-row justify-between items center">
-        <div className="my-auto">
-          <Button
-            onClick={() => {
-              window.open(experienceData.learnMoreUrl);
-            }}
-          >
-            Learn More
-          </Button>
-        </div>
+      <div className="pt-4 flex flex-row items center">
+        {experienceData.learnMoreUrl && (
+          <div className="my-auto">
+            <Button
+              onClick={() => {
+                window.open(experienceData.learnMoreUrl);
+              }}
+            >
+              Learn More
+            </Button>
+          </div>
+        )}
         <div className="ml-auto">
           <TechLogoList logos={experienceData.technologies} size={40} />
         </div>
